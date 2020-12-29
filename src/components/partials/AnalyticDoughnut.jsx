9764d7a5a043e6c132913b47005f2c1500ac48a9
NaimuddinNaimuddin <naimuddin5409@gmail.com>
2020-12-29 18:37:23 +0530

    added graphs

--- src/components/partials/AnalyticDoughnut.jsx
+++ src/components/partials/AnalyticDoughnut.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React,{ useState , useEffect} from "react";
 import Card from "react-bootstrap/Card";
 import { Doughnut } from "react-chartjs-2";
 
 export default function AnalyticDoughnut() {
-  const data = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+  const [values , valuesUpdate] = useState([])
+  console.log(values , "valy")
+  const dataLocal = {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     datasets: [
       {
         label: "# of Votes",
-        data: [35, 19, 30, 21, 32, 43],
+        data: values,
         backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
           "rgba(255, 99, 132, 0.2)",
           "rgba(54, 162, 235, 0.2)",
           "rgba(255, 206, 86, 0.2)",
@@ -24,17 +32,46 @@ export default function AnalyticDoughnut() {
           "rgba(75, 192, 192, 1)",
           "rgba(153, 102, 255, 1)",
           "rgba(255, 159, 64, 1)",
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
         ],
         borderWidth: 1,
       },
     ],
   };
+  // const [data , dataUpdate] = useState(dataLocal)
+  // console.log(data , "data")
+  useEffect(()=>{
+    const url= `https://bharatjaldispenser.herokuapp.com/delivery/graph`
+    fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application.json",
+        "x-access-token":  localStorage.getItem("token"),
+      },
+    })
+      .then((response) => response.json())
+      .then((res)=>{
+            // console.log(res?.deliveries[0]?.sum)
+            console.log(res?.deliveries[0]?.sum_of_amount)
+            valuesUpdate(res?.deliveries[0]?.sum_of_amount)
+            // dataUpdate(dataLocal)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+  },[])
   return (
     <Card>
       <Card.Title className="text-center pt-3">Monthly Transactions </Card.Title>
       <Card.Body>
         <Doughnut
-          data={data}
+          data={dataLocal}
           width={100}
           height={300}
           options={{ maintainAspectRatio: false }}
--- src/components/partials/AnalyticPie.jsx
+++ src/components/partials/AnalyticPie.jsx
@@ -1,34 +1,70 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Card from "react-bootstrap/Card";
 import { Pie } from "react-chartjs-2";
 
 export default function AnalyticPie() {
+  const [values , valuesUpdate] = useState([])
   const data = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     datasets: [
       {
         // label: "Total Transactions",
-        data: [31500, 42190, 26360, 22638, 28332, 83833],
+        data: values,
         backgroundColor: [
           "rgba(255, 99, 132, 0.2)",
           "rgba(54, 162, 235, 0.2)",
           "rgba(255, 206, 86, 0.2)",
           "rgba(75, 192, 192, 0.2)",
           "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
+          "rgba(23, 159, 64, 0.2)",
+          "rgba(205, 99, 132, 0.2)",
+          "rgba(54, 162, 35, 0.2)",
+          "rgba(225, 206, 86, 0.2)",
+          "rgba(175, 192, 92, 0.2)",
+          "rgba(253, 102, 255, 0.2)",
+          "rgba(244, 109, 64, 0.2)",
         ],
         borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(23, 159, 64, 0.2)",
+          "rgba(205, 99, 132, 0.2)",
+          "rgba(54, 162, 35, 0.2)",
+          "rgba(225, 206, 86, 0.2)",
+          "rgba(175, 192, 92, 0.2)",
+          "rgba(253, 102, 255, 0.2)",
+          "rgba(244, 100, 60, 0.2)",
         ],
         borderWidth: 1,
       },
     ],
   };
+
+  useEffect(()=>{
+    const url= `https://bharatjaldispenser.herokuapp.com/delivery/graph`
+    fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application.json",
+        "x-access-token":  localStorage.getItem("token"),
+      },
+    })
+      .then((response) => response.json())
+      .then((res)=>{
+            // console.log(res?.deliveries[0]?.sum)
+            console.log(res?.deliveries[0]?.sum_of_ltrs)
+            valuesUpdate(res?.deliveries[0]?.sum_of_ltrs)
+            // dataUpdate(dataLocal)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+  },[])
+
   return (
     <Card>
       <Card.Title className="text-center pt-3">Monthly Consumption</Card.Title>
